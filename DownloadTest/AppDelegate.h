2a9352f8989fbfd4d466d9d0fd25ae78fdf8b5a7
Joanna Zatorska <zatorska.j@gmail.com>
2017-07-11 18:52:42 +0200

    Initial Commit

---
+++ DownloadTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadTest
+//
+//  Created by MacBook Pro on 11.07.2017.
+//  Copyright © 2017 Joanna Zatorska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloadTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadTest
+//
+//  Created by MacBook Pro on 11.07.2017.
+//  Copyright © 2017 Joanna Zatorska. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
